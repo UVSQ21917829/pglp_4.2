c9913d0b52108906f7090480610555476f1b236d
ait ihya <hajaraitihya@gmail.com>
2020-03-28 13:47:05 +0100

    correction

--- exrcice4_2/src/main/java/fr/uvsq/exrcice4_2/QuitCommand.java
+++ exrcice4_2/src/main/java/fr/uvsq/exrcice4_2/QuitCommand.java
@@ -1,10 +1,17 @@
 package fr.uvsq.exrcice4_2;
 
-public class QuitCommand implements GenericCommand{
+public class QuitCommand implements Interfacecommand{
+
+	private Interpreter interpretr;
+	
+	public QuitCommand(Interpreter interpretr) {
+	
+		this.interpretr = interpretr;
+	}
 
 	public void apply() {
 		// TODO Auto-generated method stub
-		
+		this.interpretr.quit();
 	}
 
 }
--- exrcice4_2/src/main/java/fr/uvsq/exrcice4_2/Undo.java
+++ exrcice4_2/src/main/java/fr/uvsq/exrcice4_2/Undo.java
@@ -1,9 +1,17 @@
 package fr.uvsq.exrcice4_2;
 
-public class Undo implements GenericCommand {
+public class Undo implements Interfacecommand {
 
+	
+	private Interpreter interpreter;
+
+	public Undo(Interpreter interpreter) {
+		this.interpreter = interpreter;
+	}
 	public void apply() {
 		// TODO Auto-generated method stub
+		
+		this.interpreter.undo();
 		
 	}
 
