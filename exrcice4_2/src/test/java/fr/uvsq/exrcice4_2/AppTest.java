ce4eca7b5c3331bba878d8bc965c71a3e54eb5d1
ait ihya <hajaraitihya@gmail.com>
2020-03-28 14:18:24 +0100

    ajouter un test dinitilisation

--- exrcice4_2/src/test/java/fr/uvsq/exrcice4_2/AppTest.java
+++ exrcice4_2/src/test/java/fr/uvsq/exrcice4_2/AppTest.java
@@ -1,38 +1,24 @@
 package fr.uvsq.exrcice4_2;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
 
 /**
- * Unit test for simple App.
+ * Unit test for simple .
  */
 public class AppTest 
-    extends TestCase
+ 
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+    
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+	MoteurRPN moueur;
+	@Before
+	public void initialization() {
+    	this.moueur = new MoteurRPN();
     }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+   
 }
