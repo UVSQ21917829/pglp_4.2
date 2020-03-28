3772f0d26d0f65e4302667866cc845d088768de3
ait ihya <hajaraitihya@gmail.com>
2020-03-28 15:08:45 +0100

    test moins

--- exrcice4_2/src/test/java/fr/uvsq/exrcice4_2/OperationTest.java
+++ exrcice4_2/src/test/java/fr/uvsq/exrcice4_2/OperationTest.java
@@ -42,6 +42,24 @@ public class OperationTest {
 		
 		
 	}
+	// Le test de l'oeration moins
+	@Test
+	public void testMoins() {
+		double val1=9.0,val2=3.0;
+		
+		CommandStore store = new CommandStore((double) val1,mot);
+		
+		store.apply();
+		store.setNumber(val2);
+		store.apply();
+		ApplicationCom cm= new ApplicationCom(Operation.Moins,mot);
+		cm.apply();
+		assertEquals(Double.valueOf(mot.operandes.get(0)), Double.valueOf(6.0));
+		
+		
+	}
+
+	
 
 	
 
